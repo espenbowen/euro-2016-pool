dbc84c21591347c31c2961d2a20d66c2258e0f58
Espen Bowen <espen.eie.bowen@gmail.com>
2016-06-14 11:00:42 +0200

    Add files via upload

---
+++ euro2016.py
@@ -0,0 +1,19 @@
+import sys
+import random
+random.seed(2016)
+
+teams = ["Albania","Austria","Belgium","Croatia","Czech Republic","England","France","Germany","Hungary","Iceland","Italy","Northern Ireland","Poland","Portugal", "Republic of Ireland","Romania","Russia","Slovakia","Spain","Sweden","Switzerland","Turkey","Ukraine","Wales"]
+
+players = ["Patrick","Matt","Jack","Andrew","Kara","Espen"]
+
+sys.stdout.write("Team selection \n")
+for player in players:
+	sys.stdout.write("%s : "%player)
+	for _ in range(4):
+		team = teams[random.randint(0,len(teams)-1)]
+		sys.stdout.write(" %s "%team)
+		teams.remove(team)
+	sys.stdout.write("\n")
+		
+
+
